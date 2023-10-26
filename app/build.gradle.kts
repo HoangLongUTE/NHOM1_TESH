430b800a3eb5637cb3ac84d0401f7427c43aa5e5
TruyenBD <mrtruyenbd1407@gmail.com>
2023-10-27 00:55:43 +0700

    Truyen - Layout Product detail

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     // TODO: Add the dependencies for Firebase products you want to use
     // When using the BoM, don't specify versions in Firebase dependencies
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.makeramen:roundedimageview:2.3.0")
 
 
     // Add the dependencies for any other desired Firebase products
---
+++ app/src/main/java/com/example/tesh/fragment_detail_product.java
@@ -0,0 +1,14 @@
+package com.example.tesh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fragment_detail_product extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_detail_product);
+    }
+}
