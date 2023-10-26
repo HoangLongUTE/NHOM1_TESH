620e7a3f9fa32cacbb9359ece7034f7f3bc47bb6
ThuyNga <nthuynga16122003@gmail.com>
2023-10-26 15:10:06 +0700

    Nga_Notification+User

--- app/src/main/java/com/example/tesh/fragment/FragmentNotification.java
+++ app/src/main/java/com/example/tesh/fragment/FragmentNotification.java
@@ -3,12 +3,19 @@ package com.example.tesh.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.tesh.R;
+import com.example.tesh.adapter.notification_adapter;
+import com.example.tesh.model.notification_model;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +23,7 @@ import com.example.tesh.R;
  * create an instance of this fragment.
  */
 public class FragmentNotification extends Fragment {
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -25,6 +33,9 @@ public class FragmentNotification extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private RecyclerView rcview;
+    private notification_adapter noti_adapter;
 
     public FragmentNotification() {
         // Required empty public constructor
@@ -61,6 +72,46 @@ public class FragmentNotification extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+        View view = inflater.inflate(R.layout.fragment_notification, container, false);
+
+        Anhxa(view);
+        Action();
+        return view;
+
     }
+    private void Anhxa(View view){
+
+        rcview = view.findViewById(R.id.rc_view);
+    }
+    private void Action(){
+
+        noti_adapter = new notification_adapter(getActivity());
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), RecyclerView.VERTICAL, false);
+        rcview.setLayoutManager(linearLayoutManager);
+        noti_adapter.setData(getlist_notification());
+        rcview.setAdapter(noti_adapter);
+    }
+    private List<notification_model> getlist_notification() {
+        List<notification_model> notificationModels = new ArrayList<>();
+        notificationModels.add(new notification_model(R.drawable.splash_logo, "BIG UPDATE IN 10/2023", "Please update lastest versionversion", "15/10/2023"));
+        notificationModels.add(new notification_model(R.drawable.gift_box_with_a_bow, "GIFT IN 10/2023 ", "Gift free affter update  ", "14/10/2023"));
+        notificationModels.add(new notification_model(R.drawable.bag, "SELLING IN 10/2023 ", "Selling", "13/10/2023"));
+        notificationModels.add(new notification_model(R.drawable.coupon_black, "FREESHIP", "You have a voucher!!", "11/10/2023"));
+        notificationModels.add(new notification_model(R.drawable.sale_sign, "SUPER SALE IN 10/10", "Hunt for sale now!!", "10/10/2023"));
+        notificationModels.add(new notification_model(R.drawable.coupon_off20, "Shop123 give you a voucher!!", "20% off voucher", "9/10/2023"));
+        notificationModels.add(new notification_model(R.drawable.black_friday, "BLACK FRIDAY", "Shopping now!!!", "8/10/2023"));
+        notificationModels.add(new notification_model(R.drawable.super_sale, "FLASH SALE", "Shopping with hot price!!!", "7/10/2023"));
+        notificationModels.add(new notification_model(R.drawable.free_ship, "Notification", "Your voucher is about to expire", "5/10/2023"));
+        notificationModels.add(new notification_model(R.drawable.free_ship, "Warning!!!", "The product you like is about to run out", "4/10/2023"));
+        notificationModels.add(new notification_model(R.drawable.free_ship, "Warning!!!", "The product you like is about to run out", "4/10/2023"));
+        notificationModels.add(new notification_model(R.drawable.free_ship, "Warning!!!", "The product you like is about to run out", "4/10/2023"));
+        notificationModels.add(new notification_model(R.drawable.free_ship, "Warning!!!", "The product you like is about to run out", "4/10/2023"));
+        notificationModels.add(new notification_model(R.drawable.free_ship, "Warning!!!", "The product you like is about to run out", "4/10/2023"));
+        notificationModels.add(new notification_model(R.drawable.free_ship, "Warning!!!", "The product you like is about to run out", "4/10/2023"));
+
+        return notificationModels;
+
+    }
+
 }
