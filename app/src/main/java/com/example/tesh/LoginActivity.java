ddaab857e34f74f40dd05d152c26b01591d8fc8b
Le Van Binh Dinh <biskull03@gmail.com>
2023-12-10 21:57:45 +0700

    Dinh-login-signup-resetpass

--- app/src/main/java/com/example/tesh/LoginActivity.java
+++ app/src/main/java/com/example/tesh/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.tesh;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,8 +12,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.example.tesh.Cart.Cart;
+import com.example.tesh.Cart.item;
+import com.example.tesh.User.User;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,6 +31,7 @@ public class LoginActivity extends AppCompatActivity {
     TextView tv_Signup,text_forgetPassword;
     Button btnLogin, btn_facebook;
     TextInputEditText et_UserName, et_Password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,11 +44,10 @@ public class LoginActivity extends AppCompatActivity {
         et_Password=findViewById(R.id.textPassword);
         btn_facebook=findViewById(R.id.btn_facebook);
         text_forgetPassword = findViewById(R.id.text_forgetPassword);
-
         text_forgetPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, ForgotPassword.class);
+                Intent intent = new Intent(LoginActivity.this, ResetPassword.class);
                 startActivity(intent);
             }
         });
@@ -46,14 +57,37 @@ public class LoginActivity extends AppCompatActivity {
                 String username = et_UserName.getText().toString().trim();
                 String password = et_Password.getText().toString().trim();
                 if (username.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(LoginActivity.this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
-//                } else if (!isValidCredentials(username, password)) {
-//                    Toast.makeText(LoginActivity.this, "Username và password không đáp ứng yêu cầu, username và password phải bao gồm ký tự hoa, ký tự thường, chữ số", Toast.LENGTH_SHORT).show();
+                      Toast.makeText(LoginActivity.this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                  }
+                else{
+                    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Users")
+                            .child(username);
+
+                    databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                String actualPassword = dataSnapshot.child("password").getValue(String.class);
+
+                                if (password.equals(actualPassword)) {
+                                    // Mật khẩu chính xác, chuyển đến MainActivity
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(LoginActivity.this, "Mật khẩu không chính xác", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(LoginActivity.this, "Tài khoản không tồn tại", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                            // Xử lý lỗi nếu có
+                        }
+                    });
                 }
-                else {
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }
+
             }
         });
         //Chuyển sang SignUp
@@ -69,19 +103,4 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-
-
-
-
-
-//    private boolean isValidCredentials(String username, String password) {
-//        // Biểu thức chính quy để kiểm tra username và password
-//        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]+$";
-//
-//        Pattern pattern = Pattern.compile(regex);
-//        Matcher matcherUsername = pattern.matcher(username);
-//        Matcher matcherPassword = pattern.matcher(password);
-//
-//        return matcherUsername.matches() && matcherPassword.matches();
-//    }
 }
--- app/src/main/java/com/example/tesh/SignupActivity.java
+++ app/src/main/java/com/example/tesh/SignupActivity.java
@@ -1,5 +1,7 @@
 package com.example.tesh;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,7 +11,11 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.tesh.User.User;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,7 +23,7 @@ import java.util.regex.Pattern;
 public class SignupActivity extends AppCompatActivity {
 
     Button btnSignup;
-    TextInputEditText et_UserName, et_Password, et_ComfirmPassword, et_Email;
+    TextInputEditText et_UserName, et_Password, et_ComfirmPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,30 +33,44 @@ public class SignupActivity extends AppCompatActivity {
         et_UserName=findViewById(R.id.textUserName);
         et_Password=findViewById(R.id.textPassword);
         et_ComfirmPassword=findViewById(R.id.textComfirmPassword);
-        et_Email=findViewById(R.id.textEmail);
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String username = et_UserName.getText().toString().trim();
                 String password = et_Password.getText().toString().trim();
                 String comfirmpassword = et_ComfirmPassword.getText().toString();
-                String email = et_Email.getText().toString().trim();
 
-                if (username.isEmpty() || password.isEmpty() || comfirmpassword.isEmpty() || email.isEmpty()) {
+                if (username.isEmpty() || password.isEmpty() || comfirmpassword.isEmpty()) {
                     Toast.makeText(SignupActivity.this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
                 } else if (!isValidCredentials(username, password)) {
                     Toast.makeText(SignupActivity.this, "Username và password không đáp ứng yêu cầu, username và password phải bao gồm ký tự hoa, ký tự thường, chữ số", Toast.LENGTH_SHORT).show();
                 } else if (!password.equals(comfirmpassword)) {
                     Toast.makeText(SignupActivity.this, "Confirm password không khớp với password", Toast.LENGTH_SHORT).show();
-                } else if (!isValidEmail(email)) {
-                    Toast.makeText(SignupActivity.this, "Địa chỉ email không hợp lệ", Toast.LENGTH_SHORT).show();
-                } else {
+                }  else {
+
+                    User user = new User(username,password);
+                    onclickAddUser(user);
                     Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
                     startActivity(intent);
                 }
+
             }
         });
     }
+
+    private void onclickAddUser(User user) {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Users");
+
+        String pathObject = String.valueOf(user.getUsername());
+        myRef.child(pathObject).setValue(user, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(@Nullable DatabaseError error, @NonNull DatabaseReference ref) {
+                Toast.makeText(SignupActivity.this, "Sucessfull", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     private boolean isValidCredentials(String username, String password) {
         // Biểu thức chính quy để kiểm tra username và password
         String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]+$";
@@ -61,12 +81,12 @@ public class SignupActivity extends AppCompatActivity {
 
         return matcherUsername.matches() && matcherPassword.matches();
     }
-    private boolean isValidEmail(String email) {
-        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
-
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(email);
-
-        return matcher.matches();
-    }
+//    private boolean isValidEmail(String email) {
+//        String regex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+//
+//        Pattern pattern = Pattern.compile(regex);
+//        Matcher matcher = pattern.matcher(email);
+//
+//        return matcher.matches();
+//    }
 }
